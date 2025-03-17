6c8c37ec130a5fb22b632353a4c43351bbb8b3f5
Arundhati Anil Vaishnav <168985050+arundhativaishnav@users.noreply.github.com>
2025-03-17 23:47:30 +0530

    Time: 15 ms (86.54%), Space: 42.5 MB (69.19%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,28 +1,31 @@
 class Solution {
     public String longestPalindrome(String s) {
-       int n = s.length();
-       for(int length = n ;length>0;length--){
-            for(int start =0; start <= n -length ; start ++){
-                if(check(start , start + length , s)){
-                    return s.substring(start , start+length);
-                }
-            }
-
-       }
-       return "";
-
-    }
-    private boolean check(int i , int j , String s){
-        int left =i;
-        int right = j-1;
-
-        while(left<right){
-            if(s.charAt(left) != s.charAt(right)){
-                return false;
-            }
-            left++;
-            right--;
+        if (s == null || s.length() < 1) {
+            return "";
         }
-        return true;
+        int start = 0;
+        int end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = expandFromMiddle(s, i, i); // Odd length palindromes
+            int len2 = expandFromMiddle(s, i, i + 1); // Even length palindromes
+            int len = Math.max(len1, len2);
+            if (len > end - start) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+        return s.substring(start, end + 1); // Corrected to include the end index
+    }
+
+    public int expandFromMiddle(String s, int left, int right) {
+        if (s == null || left > right) {
+            return 0;
+        }
+
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+        }
+        return right - left - 1; // Corrected to return the length of the palindrome
     }
 }
