61573098d498ab273bd8be3a1e1ef6f7085410bb
Arundhati Anil Vaishnav <168985050+arundhativaishnav@users.noreply.github.com>
2025-07-21 01:22:00 +0530

    Time: 0 ms (100%), Space: 44.4 MB (30.69%) - LeetHub

---
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+//Three pointer approach that we use in in-place reversal of linked list.
+
+class Solution {
+
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode p = null;
+        ListNode q = null;
+        ListNode r = head;
+        while (r != null) {
+            if (q != null && q.val == r.val) {
+                r = r.next;
+                q.next = r;
+            } else {
+                p = q;
+                q = r;
+                r = r.next;
+            }
+        }
+        return head;
+    }
+}
